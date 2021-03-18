886e375feed43e8a59b20de13ced8377cb36614a
Kaychub <kaychubyt@gmail.com>
2021-03-18 12:35:58 -0600

    Skeleton

---
+++ src/views/Palette.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class='palette'>
+        <div class='swatches' v-for='swatch in this.$root.$data.palette' :key='swatch.id'>
+            {{ swatch.id }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Palette',
+}
+</script>
+
+<style scoped>
+
+</style>
