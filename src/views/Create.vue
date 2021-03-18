bee3e0254aa9ceee42af8a3bdf582afd1b9b37e7
Kaychub <kaychubyt@gmail.com>
2021-03-18 12:56:22 -0600

    Bare bones creation

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -1,11 +1,34 @@
 <template>
     <div class='content'>
+        <div class='swatch' v-for='color in this.colors' :key='color.id'>
+            <div class='color' :style='styleBackgroundColor(color.color)'>Hello</div>
+            <input type='text' v-model='color.color' />
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Create',
+    data: () => {
+        return {
+            colors: [
+                        {
+                            id: 1,
+                            color: '#aa00ff',
+                        },
+                        {
+                            id: 2,
+                            color: '#aa00ff'
+                        } 
+                    ],
+        }
+    },
+    methods: {
+        styleBackgroundColor(c) {
+            return 'background-color: ' + c;
+        },
+    }
 }
 </script>
 
