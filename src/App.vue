c6fff578619874bcf74e15b166420d6568bb6ec3
Kaychub <kaychubyt@gmail.com>
2021-03-18 20:26:55 -0600

    Added meta viewport

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/create">Create</router-link> |
